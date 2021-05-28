362dd8bc81fc4d3524e36063e7b0cf814fc200c3
Marlon Danilo Rocha Cardoso <marlondanrc@gmail.com>
2021-05-28 11:24:04 -0300

    Criado migration, model, dbContext para criação da tabela médico no banco de dados. Criada String de conexão com o banco de dados. Instaladas as dependências utilizadas até o momento (EntityFramework, Pomelo.EntityFrameork.MySql).

---
+++ GrupoSYM/Migrations/20210528141805_CreateMedicos.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GrupoSYM.Migrations
+{
+    public partial class CreateMedicos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Medicos",
+                columns: table => new
+                {
+                    CRM = table.Column<string>(type: "varchar(255) CHARACTER SET utf8mb4", nullable: false),
+                    Nome = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Telefone = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    ValorConsulta = table.Column<decimal>(type: "decimal(65,30)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medicos", x => x.CRM);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Medicos");
+        }
+    }
+}
---
+++ GrupoSYM/Models/Medico.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrupoSYM.Models
+{
+    public class Medico
+    {
+        [Key]
+        public string CRM { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public decimal ValorConsulta { get; set; }
+    }
+}
