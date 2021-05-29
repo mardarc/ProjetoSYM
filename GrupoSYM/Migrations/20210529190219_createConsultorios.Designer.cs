e94fa8adc69a7cc569035ad445ab0d3234d857da
Marlon <marlondanrc@gmail.com>
2021-05-29 16:06:50 -0300

    - Criadas as migrations para criação da tabela consultorios.  - Criado o model Consultorio.

--- GrupoSYM/Migrations/20210529190219_createConsultorios.Designer.cs
+++ GrupoSYM/Migrations/20210529190219_createConsultorios.Designer.cs
@@ -2,42 +2,21 @@
 using GrupoSYM.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GrupoSYM.Migrations
 {
     [DbContext(typeof(SymContext))]
-    partial class SymContextModelSnapshot : ModelSnapshot
+    [Migration("20210529190219_createConsultorios")]
+    partial class createConsultorios
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("Relational:MaxIdentifierLength", 64)
                 .HasAnnotation("ProductVersion", "5.0.6");
-
-            modelBuilder.Entity("GrupoSYM.Models.Consultorio", b =>
-                {
-                    b.Property<string>("ID")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("varchar(255)");
-
-                    b.Property<string>("Endereco")
-                        .IsRequired()
-                        .HasColumnType("varchar(200)");
-
-                    b.Property<string>("Nome")
-                        .IsRequired()
-                        .HasColumnType("varchar(100)");
-
-                    b.Property<string>("Telefone")
-                        .IsRequired()
-                        .HasColumnType("varchar(20)");
-
-                    b.HasKey("ID");
-
-                    b.ToTable("Consultorios");
-                });
 
             modelBuilder.Entity("GrupoSYM.Models.Medico", b =>
                 {
--- GrupoSYM/Migrations/20210529190450_createConsultorio.Designer.cs
+++ GrupoSYM/Migrations/20210529190450_createConsultorio.Designer.cs
@@ -2,14 +2,16 @@
 using GrupoSYM.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GrupoSYM.Migrations
 {
     [DbContext(typeof(SymContext))]
-    partial class SymContextModelSnapshot : ModelSnapshot
+    [Migration("20210529190450_createConsultorio")]
+    partial class createConsultorio
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
